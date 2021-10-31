009bf53790797b589f63117bbe30f826b0117fd4
Azzeddine Lahmar <azzeddine21.lahmar@gmail.com>
2021-11-01 00:48:22 +0100

    d

---
+++ app/src/main/java/azzeddine/lahmar/apprentissageandroid/MainActivity2.java
@@ -0,0 +1,30 @@
+package azzeddine.lahmar.apprentissageandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    private TextView myText;
+    private Button myButton1;
+    private Button myButton2;
+    private Button myButton3;
+    private Button myButton4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        myText = findViewById(R.id.main2_textview_question);
+        myButton1 = findViewById(R.id.main2_button_1);
+        myButton2 = findViewById(R.id.main2_button_2);
+        myButton3 = findViewById(R.id.main2_button_3);
+        myButton4 = findViewById(R.id.main2_button_4);
+        myButton1.setEnabled(false);
+        myButton2.setEnabled(false);
+        myButton3.setEnabled(false);
+        myButton4.setEnabled(false);
+    }
+}
